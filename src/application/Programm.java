cdc42f08f31be7d378eb0f992d3ca03e14591f6e
Isac Luciano <isacluciano@gmail.com>
2019-05-05 13:10:44 +0200

    Chess layer and printig board

--- src/application/Programm.java
+++ src/application/Programm.java
@@ -1,13 +1,15 @@
 package application;
 
-import boardgame.Board;
+import chess.ChessMatch;
 
 
 public class Programm {
 
 	public static void main(String[] args) {
 		
-		Board board = new Board(8, 8);
+		ChessMatch chessMatch = new ChessMatch ();
+		UI.printBoard(chessMatch.getPieces());
+		
 	}
 
 }
--- src/boardgame/Piece.java
+++ src/boardgame/Piece.java
@@ -2,5 +2,16 @@ package boardgame;
 
 public class Piece {
 
-	
+	protected Position position;
+	private Board board;
+
+	public Piece(Board board) {
+		this.board = board;
+		position = null;
+	}
+
+	protected Board getBoard() { //somente classes e subclasses do pacote podem acessar. Protecao do sistema
+		return board;
+	}
+
 }
